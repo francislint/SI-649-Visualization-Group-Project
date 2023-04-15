80956e85a5d726bbb8e33e6cb2a435985032534f
Tong Lin <tonglin@umich.edu>
2023-04-15 17:18:44 -0400

    add homepage

---
+++ pages/HOMEPAGE.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import altair as alt
+import streamlit as st
+import time
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("## Social Media Platforms Comparisons! 👋")
+
+st.sidebar.success("Select a Visualization Above.")
+
