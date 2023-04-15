cda3f9f5aec43156002a941b06482d3ad0be901c
Tong Lin <106212108+francislint@users.noreply.github.com>
2023-04-15 17:15:09 -0400

    Update INFLUENCER_DISTRIBUTION.py

--- pages/INFLUENCER_DISTRIBUTION.py
+++ pages/INFLUENCER_DISTRIBUTION.py
@@ -7,7 +7,7 @@ st.markdown("### Influencer Distribution")
 
 # st.sidebar.header("Mapping Demo")
 st.write(
-    """To view the dynamic visualization, please visit [INS and YOTUBE INFLUENCERS DISTRIBUTION](https://public.tableau.com/app/profile/tong.lin/viz/audiencemapsforinsandyoutube/Dashboard1)"""
+    """To view the dynamic visualization, please visit [INS and YOUTUBE INFLUENCERS DISTRIBUTION](https://public.tableau.com/app/profile/tong.lin/viz/audiencemapsforinsandyoutube/Dashboard1)"""
 )
 
 st.image('imgs/map.jpeg')
