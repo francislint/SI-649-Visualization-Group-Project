ceff7d629e8abe0388abc2b7040ffe65f04f66f5
Julienne_Hu <98298000+JulienneHu@users.noreply.github.com>
2023-04-17 22:04:29 -0400

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -16,14 +16,15 @@ st.write('### Brief Caption')
 
 st.write('In this project, our focus is on exploring the top 1000 influencers across diverse social media platforms, i.e. **Instagram**, **YouTube**, and **TikTok**, with a specific emphasis on the following aspects: ')
 st.write('''
-- The **category distribution** of influencers and the **number of followers in each category** on Instagram & YouTube'
+- The **category distribution** of influencers and the **number of followers in each category** on Instagram & YouTube
 - The **audience engagement** (e.g. average views, averege likes and average comments) on Tiktok & Instagram
 - The **fluctuations in the follower count** of the leading 5 influencers on all platforms over the past 3 years
 - The **influencer distribution** for Instagram & YouTube
-''')
+        ''')
 st.write('### Data Source')
 st.write('')
 st.write('### Methods')
-st.write('Pandas for Data Processing')
-st.write('Seaborn for Static Visualization')
-st.write('')
+st.write('''
+- Pandas for Data Processing
+- Seaborn for Static Visualization
+        ‘’‘)
